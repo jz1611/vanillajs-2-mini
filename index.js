1014595549255c29bfc4019ab4cdf02d2125c34b
Jeff Zivkovic <jzivkovic1611@gmail.com>
2019-11-13 14:50:38 -0700

    Completed main project

---
+++ index.js
@@ -0,0 +1,30 @@
+function addTodo(event) {
+    event.preventDefault();
+    const listItem = document.createElement("li");
+    const todoInput = document.getElementById("item").value;
+    listItem.innerText = todoInput;
+    listItem.addEventListener("click", completeTodo);
+
+    const itemButton = document.createElement("button");
+    itemButton.innerText = 'x';
+    itemButton.addEventListener("click", removeTodo);
+    listItem.append(itemButton);
+
+    document.querySelector("ul").appendChild(listItem);
+}
+
+document.querySelector("form").addEventListener("submit", addTodo);
+
+function removeTodo(event) {
+    event.target.parentNode.remove();
+}
+
+function completeTodo(event) {
+    const ariaChecked = event.target.getAttribute("aria-checked");
+
+    if (ariaChecked !== "true") {
+        event.target.setAttribute("aria-checked", "true");
+    } else {
+        event.target.setAttribute("aria-checked", "false");
+    }
+}
